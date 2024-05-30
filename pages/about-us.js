e72d6443dec6cdee82de986fbc46e7345131e9a4
JPivista <jpivistasolutions@gmail.com>
2024-05-30 11:38:38 +0530

    about us know more CTA button padding px-3 and px-5 is updated

--- pages/about-us.js
+++ pages/about-us.js
@@ -147,9 +147,9 @@ function ContainerExample() {
             <p className="mb-5 mx-3" style={{ color: '#E1E1E1' }}>Swasti, a member of the Catalyst family, was founded in 2004. Swasti co-designs demonstrable and scalable solution models for businesses in partnership with communities on the ground and influences policies nationally and internationally. Swasti is helping communities, governments, and partners shape safe, secure, healthy and more prosperous lives across the world through various initiatives that reach over 400,000 people.
               Over the past eight years, Walmart and Swasti have shared a meaningful partnership which now includes the Walmart Vriddhi program.</p>
 
-              {/* CTA Button was Changed as mx-5 to mx-3 */}
-              
-            <Link href="https://swastihc.org/" target="_blank" className="ab-button mb-4 mt-5 bogle-medium mx-3">Know more</Link>
+            {/* CTA Button was Changed as mx-5 to mx-3 */}
+
+            <Link href="https://swastihc.org/" target="_blank" className="ab-button mb-4 mt-5 bogle-medium mx-3 py-3 px-5">Know more</Link>
           </Col>
           <Col className="p-0 d-flex justify-content-end">
 
