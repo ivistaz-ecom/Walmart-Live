64b4f3a0b37a6cefec2cd7be652fb03018b6dd1c
Bhavankumarg <gbhavanbkr@gmail.com>
2024-09-11 16:12:05 +0530

    404

--- pages/404.js
+++ pages/404.js
@@ -60,7 +60,7 @@ function Error() {
           <Col sm={12} lg={7}>
             <h2 className='fs-1 bogle-medium walmart-default'>Accept our appologies!</h2>
             <p className='fs-3 mb-5'>The page you were looking for doesn't exists.You may have misstyped the address or the page may have moved.</p>
-            <Link href="" className='error-btn fs-5 text-white '>Go to Home page</Link>
+            <Link href="/" className='error-btn fs-5 text-white '>Go to Home page</Link>
           </Col>
           
 
